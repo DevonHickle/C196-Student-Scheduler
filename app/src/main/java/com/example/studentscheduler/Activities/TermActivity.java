4c6907fecdf7f933d549e16a896111d8489a81c0
devonhickle <devonhickle@yahoo.com>
2023-01-03 08:32:32 -0600

    Working on some UI elements

---
+++ app/src/main/java/com/example/studentscheduler/Activities/TermActivity.java
@@ -0,0 +1,7 @@
+package com.example.studentscheduler.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TermActivity extends AppCompatActivity {
+    public static final String EXTRA_ID = "com.example.student-scheduler.activities.ID";
+}
