c5af3c7dbd8fe9453a7ea4b825b18d0ef0fa2c54
devonhickle <devonhickle@yahoo.com>
2023-01-07 12:26:37 -0600

    Working on course details, still pending ui

--- app/src/main/java/com/example/studentscheduler/Activities/CourseDetailActivity.java
+++ app/src/main/java/com/example/studentscheduler/Activities/CourseDetailActivity.java
@@ -1,6 +1,6 @@
 package com.example.studentscheduler.Activities;
 
 public class CourseDetailActivity {
-    public static final Object STATUS_IN_PROGRESS = 2;
-    public static final Object STATUS_COMPLETED = 1;
+    public static final int STATUS_IN_PROGRESS = 2;
+    public static final int STATUS_COMPLETED = 1;
 }
--- app/src/main/java/com/example/studentscheduler/Entities/CourseEntity.java
+++ app/src/main/java/com/example/studentscheduler/Entities/CourseEntity.java
@@ -1,12 +1,120 @@
 package com.example.studentscheduler.Entities;
 
-import androidx.room.Entity;
+import static androidx.room.ForeignKey.CASCADE;
 
-@Entity(tableName = "courses")
-public class CourseEntity {
-    public CourseEntity(int i, String s, String s1, String s2) {
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "courses",
+    foreignKeys = @ForeignKey(entity = TermEntity.class,
+            parentColumns = "id",
+            childColumns = "termID",
+            onDelete = CASCADE
+    ))
+public class CourseEntity extends com.example.studentscheduler.Util.Entity {
+    @PrimaryKey(autoGenerate = true)
+
+    private int id;
+    private int termID;
+    private String title;
+    private String startDate;
+    private String endDate;
+    private boolean alert;
+    private int status;
+    private String MentorName;
+    private String MentorPhone;
+    private String MentorEmail;
+
+    public CourseEntity(int termID, String title, String startDate, String endDate, boolean alert, int status, String MentorName, String MentorPhone, String MentorEmail) {
+        this.termID = termID;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.alert = alert;
+        this.status = status;
+        this.MentorName = MentorName;
+        this.MentorPhone = MentorPhone;
+        this.MentorEmail = MentorEmail;
     }
 
-    public CourseEntity(int i, String s, String s1, String s2, boolean b, Object statusCompleted, String devon, String s3, String s4) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTermID() {
+        return termID;
+    }
+
+    public void setTermID(int termID) {
+        this.termID = termID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isAlert() {
+        return alert;
+    }
+
+    public void setAlert(boolean alert) {
+        this.alert = alert;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMentorName() {
+        return MentorName;
+    }
+
+    public void setMentorName(String MentorName) {
+        this.MentorName = MentorName;
+    }
+
+    public String getMentorPhone() {
+        return MentorPhone;
+    }
+
+    public void setMentorPhone(String MentorPhone) {
+        this.MentorPhone = MentorPhone;
+    }
+
+    public String getMentorEmail() {
+        return MentorEmail;
+    }
+
+    public void setMentorEmail(String MentorEmail) {
+        this.MentorEmail = MentorEmail;
     }
 }
