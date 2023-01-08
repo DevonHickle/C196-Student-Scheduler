a21df3745b0c9a8abf2e626212d272693a263b29
devonhickle <devonhickle@yahoo.com>
2023-01-08 08:58:16 -0600

    Working on entities

--- app/src/main/java/com/example/studentscheduler/Entities/AssessmentEntity.java
+++ app/src/main/java/com/example/studentscheduler/Entities/AssessmentEntity.java
@@ -4,13 +4,79 @@ import static androidx.room.ForeignKey.CASCADE;
 
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "assessments",
         foreignKeys = @ForeignKey(entity = CourseEntity.class,
                 parentColumns = "id",
                 childColumns = "courseID",
-                onDelete = CASCADE))
-public class AssessmentEntity {
-    public AssessmentEntity(int i, String s, int i1, String s1, boolean b) {
+                onDelete = CASCADE
+        ))
+public class AssessmentEntity extends com.example.studentscheduler.Util.Entity {
+    @PrimaryKey(autoGenerate = true)
+
+    private int id;
+    private int courseID;
+    private String name;
+    private int type;
+    private String goalDate;
+    private boolean alert;
+
+    public AssessmentEntity(int courseID, String name, int type, String goalDate, boolean alert) {
+        this.courseID = courseID;
+        this.name = name;
+        this.type = type;
+        this.goalDate = goalDate;
+        this.alert = alert;
+    }
+
+    // Getters
+    public int getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getGoalDate() {
+        return goalDate;
+    }
+
+    public boolean getAlert() {
+        return alert;
+    }
+
+    // Setters
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setCourseID( int courseID) {
+        this.courseID = courseID;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setGoalDate (String goalDate) {
+        this.goalDate = goalDate;
+    }
+
+    public void setAlert(boolean alert) {
+        this.alert = alert;
     }
 }
