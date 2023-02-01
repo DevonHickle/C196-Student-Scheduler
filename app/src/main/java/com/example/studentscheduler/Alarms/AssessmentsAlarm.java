e257f1c9ad5acf74ebc3a738aeb24fafce480569
devonhickle <devonhickle@yahoo.com>
2023-02-01 07:17:05 -0600

    Worked on alarms, need to add paths

--- app/src/main/java/com/example/studentscheduler/Alarms/AssessmentsAlarm.java
+++ app/src/main/java/com/example/studentscheduler/Alarms/AssessmentsAlarm.java
@@ -1,4 +1,36 @@
 package com.example.studentscheduler.Alarms;
 
-public class AssessmentsAlarm {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.studentscheduler.R;
+
+public class AssessmentsAlarm extends BroadcastReceiver {
+    // TODO: add paths
+    public static final String ALARM_NOTIFICATION_TITLE = "";
+    public static final String ALARM_NOTIFICATION_TEXT = "";
+    public static final String ALARM_NOTIFICATION_TYPE = "";
+    private static final int NOTIFY_ID_ASSESSMENT_ALARM = 1;
+    private static final String ASSESSMENT_CHANNEL_ID_ALARMS = "CHANNEL_ID_COURSE_ALARMS";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context.getApplicationContext());
+
+        String notificationTitle = intent.getStringExtra(ALARM_NOTIFICATION_TITLE);
+        String notificationType = intent.getStringExtra(ALARM_NOTIFICATION_TYPE);
+        String notificationText = intent.getStringExtra(ALARM_NOTIFICATION_TEXT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext(), ASSESSMENT_CHANNEL_ID_ALARMS)
+                .setSmallIcon(R.drawable.ic_add_white_24dp)
+                .setContentTitle(intent.getStringExtra(ALARM_NOTIFICATION_TITLE))
+                .setContentText(notificationTitle + " " + notificationType + " upcoming assessment on " + notificationText)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManagerCompat.notify(NOTIFY_ID_ASSESSMENT_ALARM, builder.build());
+    }
 }
--- app/src/main/java/com/example/studentscheduler/Alarms/CoursesAlarm.java
+++ app/src/main/java/com/example/studentscheduler/Alarms/CoursesAlarm.java
@@ -1,4 +1,34 @@
 package com.example.studentscheduler.Alarms;
 
-public class CoursesAlarm {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.studentscheduler.R;
+
+public class CoursesAlarm extends BroadcastReceiver {
+    // TODO: Add paths
+    public static final String ALARM_NOTIFICATION_TITLE = "";
+    public static final String ALARM_NOTIFICATION_TEXT = "";
+    public static final String COURSE_CHANNEL_ID_ALARMS = "CHANNEL_ID_COURSE_ALARMS";
+    public static final int NOTIFY_ID_COURSE_ALARM = 0;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context.getApplicationContext());
+
+        String notificationTitle = intent.getStringExtra(ALARM_NOTIFICATION_TITLE);
+        String notificationText = intent.getStringExtra(ALARM_NOTIFICATION_TEXT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext(), COURSE_CHANNEL_ID_ALARMS)
+                .setSmallIcon(R.drawable.ic_add_white_24dp)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationTitle + " " + notificationText)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManagerCompat.notify(NOTIFY_ID_COURSE_ALARM, builder.build());
+    }
 }
