722b7f40473d2607745c2b49add4c7e0accfb137
devonhickle <devonhickle@yahoo.com>
2023-02-22 20:26:05 -0600

    Missed add-edit course functionality, working on that, troubleshooting adapter issue

--- app/src/main/java/com/example/studentscheduler/Activities/AddEdit/AddEditCourses.java
+++ app/src/main/java/com/example/studentscheduler/Activities/AddEdit/AddEditCourses.java
@@ -1,6 +1,17 @@
 package com.example.studentscheduler.Activities.AddEdit;
 
-public class AddEditCourses {
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.studentscheduler.R;
+
+import java.util.Calendar;
+
+public class AddEditCourses extends AppCompatActivity {
     // TODO: Add Paths
     public static final int REQUEST_ADD_COURSE = 1;
     public static final String EXTRA_COURSE_TITLE = "";
@@ -12,5 +23,23 @@ public class AddEditCourses {
     public static final String EXTRA_COURSE_INSTRUCTOR_EMAIL = "";
     public static final String EXTRA_COURSE_INSTRUCTOR_PHONE = "";
     public static final String EXTRA_COURSE_ID = "";
-    public static final String DATE_FORMAT = "";
+    public static final String DATE_FORMAT = "MM/dd/yyyy";
+    public static final int REQUEST_EDIT_COURSE = 2;
+
+    private Calendar startDate;
+    private Calendar endDate;
+
+    private EditText editTitle;
+    private EditText editStartDate;
+    private EditText editEndDate;
+    private CheckBox courseAlarmCheck;
+    private RadioGroup editCourseStatus;
+    private EditText editInstructorName;
+    private EditText editInstructorPhone;
+    private EditText editInstructorEmail;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_edit_course);
+    }
 }
